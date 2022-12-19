99849c0cddafc76f96c034df768189efa58bc9b4
eric.yoo <yoosungung@gmail.com>
2022-12-19 22:07:57 +0900

    move folder

--- dags/local/load_mart.py
+++
@@ -1,9 +0,0 @@
-import k8s_exec
-import s3_exec
-
-def main():
-    res = k8s_exec.exec_commands('load_mart', 'ls')
-    s3_exec.s3_save(snakemake.output[0], res)
-
-if __name__ == '__main__':
-    main()
